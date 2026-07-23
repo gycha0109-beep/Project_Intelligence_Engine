3a0d3a9f6856d2494933f3a57fa9517a12c2bb10
gycha0109-beep <gycha0109@gmail.com>
2026-07-24 07:05:43 +0900

    export GitHub runner internals

--- src/review_system/github/__init__.py
+++ src/review_system/github/__init__.py
@@ -1,8 +1,12 @@
 """GitHub integration internals with compatibility exports."""
 
+from .runner import CommandResult, GitHubCLI, GitHubCLIError
 from .target import PullRequestTarget, normalize_repository, parse_pr_target, repository_argument
 
 __all__ = [
+    "CommandResult",
+    "GitHubCLI",
+    "GitHubCLIError",
     "PullRequestTarget",
     "normalize_repository",
     "parse_pr_target",
